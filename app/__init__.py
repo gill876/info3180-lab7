e2ecddee554816d33bf9b559c403c430da56b016
Cargill Seiveright <microcargill@hotmail.com>
2020-04-29 23:58:35 -0500

    add form

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,11 @@
 from flask import Flask
+from flask_wtf.csrf import CSRFProtect
+import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'v\xf9\xf7\x11\x13\x18\xfaMYp\xed_\xe8\xc9w\x06\x8e\xf0f\xd2\xba\xfd\x8c\xda'
+
+csrf = CSRFProtect(app)
+csrf.init_app(app)
 
 from app import views
--- app/forms.py
+++ app/forms.py
@@ -1,1 +1,11 @@
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileRequired, FileAllowed
+from wtforms import StringField, TextAreaField, SelectField
+from wtforms.validators import DataRequired, Email
 
+class UploadForm(FlaskForm):
+    biography = TextAreaField('description', validators=[DataRequired()])
+    profile_img = FileField('photo', validators=[
+        FileRequired(),
+        FileAllowed(['jpg', 'png', 'jpeg', 'Images only!'])
+    ])
